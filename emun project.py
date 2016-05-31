c157d0dbdc43d3b987042537a382b7e79f1b8301
saritbk <saritbk@gmail.com>
2016-06-01 00:44:34 +0300

    Update emun project.py

--- emun project.py
+++ emun project.py
@@ -14,11 +14,13 @@ class Delay(enum.Enum):
 
 
 class Trip:
-    def __init__(self, route_id, agent, stop_id, time, direction, delay_type, total_delay):
+    def __init__(self, feed_id, route_id, agent, stop_id, arrival_time, planned_time, direction, delay_type, total_delay):
+        self.siri_feed_id = feed_id
         self.route_id = route_id
         self.agent = agent
         self.stop_id = stop_id
-        self.time = time
+        self.arrival_time = arrival_time
+        self.planned_time = planned_time
         self.direction = direction
         self.delay_type = delay_type
         self.total_delay = total_delay
@@ -27,16 +29,21 @@ class Trip:
 def diffrence_between_two_trips(arrival_time, list_of_times):
     """input: route_id of a certain route and arrival_time. output: the difference between current arrival time and next arrival time (of the same route)
         notice that if it is the last trip of this line that day, the function returns the diffrence between one-before-last trip and current trip"""
-    # index of current arrival time
-    pos = list_of_times.index(arrival_time)
+    # pos = index of current arrival time
+    try:
+        pos = list_of_times.index(arrival_time.time())
+    except ValueError as e:
+        print(e)
+        return -1
     current_element = list_of_times[pos]
     if pos == len(list_of_times):
         prev_element = list_of_times[pos - 1]
         difference = current_element - prev_element
     else:
         next_element = list_of_times[pos + 1]
-        difference = next_element - arrival_time
-    return difference
+        difference = dt.datetime.combine(dt.date.today(), next_element) - dt.datetime.combine(dt.date.today(), arrival_time.time())
+        # difference = next_element - arrival_time.time()
+    return difference.seconds/60
 
 
 def delay_percent_calculation(delay_in_minute,difference):
@@ -71,7 +78,7 @@ def temp_function(delay_in_minutes):
 
 
 if __name__ == '__main__':
-    is_debug = True
+    is_debug = False
     # get current system time
     current_date = dt.datetime.now()
     # connect to database
@@ -81,22 +88,32 @@ if __name__ == '__main__':
     c = connection.cursor(as_dict = True)
     trip_list = []
 
-    c.execute('SELECT LineRef, OperatorRef, StopRef, Actual_Arrival, DirectionRef FROM siri_feeds WHERE Actual_Arrival < GETDATE()')
+    c.execute('SELECT siri_feed_id, insert_date, LineRef, OperatorRef, StopRef, Actual_Arrival, planned_gtfs, DirectionRef, Delay_type, TotalDelay FROM siri_feeds WHERE Actual_Arrival < GETDATE()')
     row = c.fetchone()
     while row:
+        if row["insert_date"] < current_date - dt.timedelta(days=1):
+            row = c.fetchone()
+            continue
+        feed_id = row["siri_feed_id"]
         route_id = row["LineRef"]
         agent = row["OperatorRef"]
         stop_id = row["StopRef"]
         arrival_time = row["Actual_Arrival"]
+        planned_time = row["planned_gtfs"]
         direction = row["DirectionRef"]
         if not is_debug:
             delay_type = row["Delay_type"]
+            if delay_type not in ['OT', 'BM', 'SM', 'miss']:
+                row = c.fetchone()
+                continue
+            x=1
         if is_debug:
             total_delay = random.randrange(0, 10, 1)
             delay_type = temp_function(total_delay)
         else:
-            total_dalay = row["Total_Delay"]
-        new_trip = Trip(route_id, agent, stop_id, arrival_time, direction, delay_type, total_delay)
+            total_delay = row["TotalDelay"]
+            x=1
+        new_trip = Trip(feed_id, route_id, agent, stop_id, arrival_time, planned_time, direction, delay_type, total_delay)
         trip_list.append(new_trip)
         row = c.fetchone()
 
@@ -116,26 +133,39 @@ if __name__ == '__main__':
 
     if trip_list:
         old_route_id = trip_list[0].route_id
-        list_of_times = [time.strptime(x, "%H:%M:%S") for x in frequency_by_routeID[old_route_id]]
+        list_of_times = [dt.datetime.strptime(x, "%H:%M:%S").time() for x in frequency_by_routeID[old_route_id]]
         # list_of_times = frequency_by_routeID[old_route_id]
         for t in trip_list:
             if t.route_id != old_route_id:
-                list_of_times = [time.strptime(x, "%H:%M:%S") for x in frequency_by_routeID[old_route_id]]
+                list_of_times = [dt.datetime.strptime(x, "%H:%M:%S").time() for x in frequency_by_routeID[old_route_id]]
                 old_route_id = t.route_id
             # calculate planned time by substracting total_dalay from Actual_Time
-            planned_time = t.time - dt.timedelta(minutes = total_delay)
+            # planned_time = t.arrival_time - dt.timedelta(minutes = total_delay)
             if is_debug:
                 delay_percent = random.uniform(0,1)
             else:
-                line_frequency = diffrence_between_two_trips(planned_time, list_of_times)
-                delay_percent = delay_percent_calculation(t.total_delay, line_frequency)
-            # date, agency_id, route_id, stop_id, planned_time, actual_time, delay, delay_type
-            new_row = (current_date.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], str(t.agent), t.route_id, t.stop_id, planned_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], t.time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], t.total_delay, t.delay_type.name, delay_percent)
-            try:
-                c.execute("INSERT INTO Reports VALUES{0}".format(new_row))
-                continue
-            except pymssql.StandardError as e:
-                print(e)
+                line_frequency = diffrence_between_two_trips(t.planned_time, list_of_times)
+                if line_frequency >= 0:
+                    delay_percent = delay_percent_calculation(t.total_delay, line_frequency)
+                    try:
+                        new_row = (current_date.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], str(t.agent), t.route_id, t.stop_id, t.planned_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], t.arrival_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], t.total_delay, t.delay_type, float(delay_percent))
+                    except ValueError as e:
+                        print(e)
+                    try:
+                        c.execute("INSERT INTO Reports VALUES{0}".format(new_row))
+                        continue
+                    except pymssql.StandardError as e:
+                        printdelay_percent = delay_percent_calculation(t.total_delay, line_frequency)
+                    # date, agency_id, route_id, stop_id, planned_time, actual_time, delay, delay_type
+                    try:
+                        new_row = (current_date.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], str(t.agent), t.route_id, t.stop_id, t.planned_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], t.arrival_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], t.total_delay, t.delay_type, float(delay_percent))
+                    except ValueError as e:
+                        print(e)
+                    try:
+                        c.execute("INSERT INTO Reports VALUES{0}".format(new_row))
+                        continue
+                    except pymssql.StandardError as e:
+                        print(e)
 
     connection.commit()
     x=1
